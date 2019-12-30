fccbbc8f1028721683cb8e199e8ea1b00a7f7482
sunnyshingala <sunny.singala@gmail.com>
2019-12-30 18:16:46 +0530

    loops

---
+++ loops.php
@@ -0,0 +1,30 @@
+<?php
+    //while loop
+    
+    // $x = 1;
+    // while($x < 5){
+    //     echo "Hi There !<br>";
+    //     $x++;
+    // }   
+    // do while loop
+    // $X = 10;
+    // do{
+    //     echo "Hot dog !<br>";
+    //     $x++;
+    // }
+    // while($x <= 5);
+    //for loop
+
+        // for($x=0; $x < 5; $x++){
+        //     echo "For Loop<br>";
+        // }
+    //for each loop
+
+            $array = array("Sunny", "Shingala", "Roy", "Jayesh");
+
+            foreach ($array as $loopdata) {
+                echo "My Name is " . $loopdata . "<br>";
+            }
+    
+    
+?>
