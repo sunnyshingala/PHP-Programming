4a0dd371c74a2f32a5394671c43aa824ef8370de
sunnyshingala <sunny.singala@gmail.com>
2020-01-01 19:51:07 +0530

    prepared statement

--- registration/includes/signup.inc.php
+++ registration/includes/signup.inc.php
@@ -7,7 +7,15 @@
     $uid = mysqli_real_escape_string($conn, $_POST['uid']);
     $password =  mysqli_real_escape_string($conn, $_POST['password']);
 
-    $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$first', '$last', '$email', '$uid', '$password');";
+    $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd)
+     VALUES (?, ?, ?, ?, ?);";
+    $stmt = mysqli_stmt_init($conn);
+    if(! mysqli_stmt_prepare($stmt, $sql)){
+        echo "Sql error"; 
+    }else{
+        mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $uid, $password);
+        mysqli_stmt_execute($stmt);
+    }
     mysqli_query($conn, $sql);
 
 
